3675ab16965764727265fc12b8c4874f90111100
caoqi95 <caoqi95@gmail.com>
2019-01-25 23:27:08 +0900

    Add filter()

---
+++ Python-practice-04-filter.py
@@ -0,0 +1,16 @@
+# 练习使用 filter() 内置函数
+
+
+def filter_nums(nums):
+
+    res = list(filter(lambda x: x >= 0, nums))
+
+    return res
+
+
+if __name__ == "__main__":
+
+    nums = list(range(-5, 5))
+    print("before filter:", nums)
+    res = filter_nums(nums)
+    print("after filter:", res)
